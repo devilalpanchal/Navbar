0a25924bd92c33bc0986e73a1c08a10b799803cd
Devilal panchal <devilalpanchal551@gmail.com>
2024-02-08 18:03:32 +0530

    add12

--- main.js
+++ main.js
@@ -17,25 +17,54 @@
 // let result = JSON.stringify(obj)
 // console.log(result)
 
-// let obj = [
-//     {
-//     page : 'home',
-//     link: 'home.html'
-// },
-//     {
-//     page : 'about',
-//     link: 'about.html'
-// },
-//     {
-//     page : 'service',
-//     link: 'service.html'
-// },
-//     {
-//     page : 'contact',
-//     link: 'contact.html'
-// }
+let obj = [
+    {
+    page : 'home',
+    link: 'home.html'
+},
+    {
+    page : 'about',
+    link: 'about.html'
+},
+    {
+    page : 'service',
+    link: 'service.html'
+},
+    {
+    page : 'contact',
+    link: 'contact.html'
+}
    
-// ]
+
+]
+
+let socialarr = [
+    {
+        href: '#',
+        icon:'<i class="fa-brands fa-facebook"></i>',
+    },
+    {
+        href: '#',
+        icon:'<i class="fa-brands fa-instagram"></i>',
+    },
+    {
+        href: '#',
+        icon:'<i class="fa-brands fa-twitter"></i>',
+    },
+    {
+        href: '#',
+        icon:'<i class="fa-brands fa-github"></i>',
+    },
+]
+
+
+let pageContainer = document.querySelector('.pages')
+let pageArray = pageObj.map((item)=>{
+    return` <li class="page"><a href="${item.link}>${item.page}">Home</a></li>`
+})
+
+pageContainer.innerHTML = pageArr.join(' ');
+
 
 let btn = document.querySelector('.menu')
 btn.addEventListener('click',()=>{
@@ -43,4 +72,13 @@ btn.addEventListener('click',()=>{
 let pages = document.querySelector('.pages')
 pages.classList.toggle('show')
 
+let icons = document.querySelector('.social-icons')
+icons.classList.toggle('show')
+
+})
+
+
+let socialContainer = document.querySelector('.social-icons')
+let socialArr = socialObj.map((item)=>{
+     return`  <li class="social-icon"><a href=${item.href}>${item.page}><i class="fa-brands fa-facebook"></i></a></li>`
 })
